98a7476833d0f27aa9170aea2b1bd197fb860a5e
izunq <izunqua@gmail.com>
2024-05-20 09:17:00 +0200

    Add project files.

---
+++ Library.ConsoleApp/BookService.cs
@@ -0,0 +1,64 @@
+﻿using Library.Domain;
+using Library.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.ConsoleApp
+{
+    public class BookService
+    {
+        private readonly BooksRepository _repository;
+
+        public BookService(BooksRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public void AddBook()
+        {
+            Console.Write("Enter title: ");
+            string title = Console.ReadLine();
+            Console.Write("Enter author: ");
+            string author = Console.ReadLine();
+            Console.Write("Enter publication year: ");
+            int publicationYear = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter ISBN: ");
+            string isbn = Console.ReadLine();
+            Console.Write("Enter products available: ");
+            int productsAvailable = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter price: ");
+            decimal price = Convert.ToDecimal(Console.ReadLine());
+
+            Book newBook = new Book(title, author, publicationYear, isbn, productsAvailable, price);
+            _repository.Insert(newBook);
+        }
+
+        public void RemoveBook()
+        {
+            Console.Write("Enter title of the book to remove: ");
+            string title = Console.ReadLine();
+            _repository.RemoveByTitle(title);
+        }
+
+        public void ListBooks()
+        {
+            var books = _repository.GetAll();
+            foreach (var book in books)
+            {
+                Console.WriteLine(book);
+            }
+        }
+
+        public void ChangeState()
+        {
+            Console.Write("Enter title of the book: ");
+            string title = Console.ReadLine();
+            Console.Write("Enter state change (e.g., -1 for decrease): ");
+            int stateChange = Convert.ToInt32(Console.ReadLine());
+            _repository.ChangeState(title, stateChange);
+        }
+    }
+}
---
+++ Library.ConsoleApp/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Library.Domain;
+using Library.Persistence;
+using Library.ConsoleApp;
+
+class Program
+{
+    static void Main()
+    {
+        BooksRepository repository = new BooksRepository();
+        BookService bookService = new BookService(repository);
+
+        Console.Write("Enter login: ");
+        string login = Console.ReadLine();
+        Console.Write("Enter password: ");
+        string password = Console.ReadLine();
+
+        if (login == "Admin" && password == "password")
+        {
+            Console.WriteLine("Access Granted");
+
+            string command;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Komendy: dodaj, usun, wypisz, zmien, dodaj zamowienie, lista zamowien, wyjdz");
+                Console.Write("Wpisz komendę: ");
+                command = Console.ReadLine();
+
+                switch (command)
+                {
+                    case "dodaj":
+                        bookService.AddBook();
+                        break;
+                    case "usun":
+                        bookService.RemoveBook();
+                        break;
+                    case "wypisz":
+                        bookService.ListBooks();
+                        break;
+                    case "zmien":
+                        bookService.ChangeState();
+                        break;
+                    case "dodaj zamowienie":
+                        Console.WriteLine("proba dodania nowego zamowienia");
+                        break;
+                    case "lista zamowien":
+                        Console.WriteLine("proba wypisania wszystkich zamowien");
+                        break;
+                    case "wyjdz":
+                        Console.WriteLine("Koniec programu");
+                        break;
+                    default:
+                        Console.WriteLine("Nie wspierana komenda");
+                        break;
+                }
+
+                if (command != "wyjdz")
+                {
+                    Console.WriteLine("Press AnyKey");
+                    Console.ReadKey();
+                }
+
+            } while (command != "wyjdz");
+        }
+        else
+        {
+            Console.WriteLine("Access Denied");
+        }
+        Console.ReadLine();
+    }
+}
